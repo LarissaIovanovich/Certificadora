a514cfda9ab329587ecf5ab6ba55da261b9baf99
JEscrivani <93689910+JEscrivani@users.noreply.github.com>
2023-11-19 10:33:53 -0300

    Add files via upload

--- certificadora/src/pages/login/index.tsx
+++ certificadora/src/pages/login/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     return (
         <>
             <Head>
-                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Projeto Bons Fluídos - Login</title>
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />
@@ -13,12 +13,12 @@ export default function Home() {
             </Head>
             <main>
                 <div className="topbar-cadastro">
-                    <div className="logo">
-                        <Link href="/">
-                            <img src="/imgs/logo.png" />
-                        </Link>
-                    </div>
-                </div>   
+                    <Link href="/" className="logo">
+                        <img src="/imgs/logo.png" />
+                        <h1>Projeto Bons Fluídos</h1>
+                    </Link>
+                </div>
+
                 <div className="login-container">
                     <h2>Login</h2>
                     <form className="login-form">
