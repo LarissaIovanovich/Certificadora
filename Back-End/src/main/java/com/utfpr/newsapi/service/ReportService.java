58c04be0468878f85856fc427263a36a71d41c0b
JEscrivani <93689910+JEscrivani@users.noreply.github.com>
2023-11-25 23:40:42 -0300

    Add files via upload

---
+++ Back-End/src/main/java/com/utfpr/newsapi/service/ReportService.java
@@ -0,0 +1,49 @@
+package com.utfpr.newsapi.service;
+
+import com.utfpr.newsapi.dto.PublishReportDTO;
+import com.utfpr.newsapi.entity.Report;
+import com.utfpr.newsapi.infra.security.TokenService;
+import com.utfpr.newsapi.repository.ReportRepository;
+import com.utfpr.newsapi.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class ReportService {
+    @Autowired
+    private ReportRepository reportRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TokenService tokenService;
+
+    public List<Report> latestNews(){
+        return reportRepository.latestMews();
+    }
+
+    public Report addNews(PublishReportDTO reportDTO,String email) {
+        Report report = new Report(
+                new Date(),
+                reportDTO.title(),
+                reportDTO.subTitle(),
+                reportDTO.body(),
+                this.userRepository.findByEmail(email)
+        );
+        return reportRepository.save(report);
+    }
+
+    public Report edit(Report report){
+        return reportRepository.save(report);
+    }
+
+    public Report showNews(Long id){
+        return reportRepository.findRef(id);
+    }
+
+}
